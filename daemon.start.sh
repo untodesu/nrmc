56d5e94b7ae273fff6fccd7485ec27d5d22f47eb
Kirill GPRB <me@gprb.su>
2023-04-01 15:35:37 +0500

    Update and rename daemon.sh to daemon.start.sh

---
+++ daemon.start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd "$(realpath $(dirname ${0}))"
+source "$(realpath $(dirname ${0}))/config/daemon.conf"
+
+if screen -list | grep -q ${server_screen}
+then
+    if [ "${1}" == "watch" ]
+    then
+        # The server is running, this means we
+        # don't need to start the server again
+        exit 0
+    else
+        screen -p 0 -S ${server_screen} -X stuff "say Server restart in 30 seconds\\015"
+        sleep 20
+        screen -p 0 -S ${server_screen} -X stuff "say Server restart in 10 seconds\\015"
+        screen -p 0 -S ${server_screen} -X stuff "save-all\\015"
+        sleep 10
+        screen -p 0 -S ${server_screen} -X stuff "kick @p Server is restarting\\015"
+        screen -p 0 -S ${server_screen} -X stuff "stop\\015"
+        sleep 10
+        screen -p 0 -S ${server_screen} -X kill > /dev/null 2> /dev/null
+    fi
+fi
+
+screen -dm -S ${server_screen} java -server ${jvm_args[*]} -jar "$(realpath $(dirname ${0}))/${server_jar}" ${server_args[*]} --nogui
+exit 0
