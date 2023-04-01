8a485c363343e090cd502d168d72ca8698bf2bdc
Kirill GPRB <me@gprb.su>
2023-04-01 21:19:11 +0500

    Update (fix sourcing) daemon.stop.sh

--- daemon.stop.sh
+++ daemon.stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd "$(realpath $(dirname ${0}))"
-source "$(realpath $(dirname ${0}))/config/daemon.conf"
+cd "$(realpath $(dirname ${BASH_SOURCE[0]}))"
+source "${PWD}/config/daemon.conf"
 
 if screen -list | grep -q ${server_screen}
 then
@@ -10,7 +10,7 @@ then
     screen -p 0 -S ${server_screen} -X stuff "say Server shutdown in 10 seconds\\015"
     screen -p 0 -S ${server_screen} -X stuff "save-all\\015"
     sleep 10
-    screen -p 0 -S ${server_screen} -X stuff "kick @p Server shut down for maintenance\\015"
+    screen -p 0 -S ${server_screen} -X stuff "kick @a Server shut down for maintenance\\015"
     screen -p 0 -S ${server_screen} -X stuff "stop\\015"
     sleep 10
     screen -p 0 -S ${server_screen} -X kill > /dev/null 2> /dev/null
