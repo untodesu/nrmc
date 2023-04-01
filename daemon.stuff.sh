66dc8bed4960357ba775667be2790d0ad610f485
Kirill GPRB <me@gprb.su>
2023-04-01 15:36:32 +0500

    Create daemon.stuff.sh

---
+++ daemon.stuff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(realpath $(dirname ${0}))"
+source "$(realpath $(dirname ${0}))/config/daemon.conf"
+
+if screen -list | grep ${server_screen}
+then
+    screen -p 0 -S ${server_screen} -X stuff "${*:1}\\015"
+    exit 0
+fi
+
+>&2 printf "server is not running\n"
+exit 1
