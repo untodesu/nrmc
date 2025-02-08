78ff876f48d168c745617037034e2344d7e0ca39
untodesu <41549137+untodesu@users.noreply.github.com>
2025-02-08 12:47:37 +0500

    Enable Git LFS for server icons

--- systemd/announce.sh
+++ systemd/announce.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-printf "%s\n" "execute at @a run playsound mineraft:item.goat_horn.sound.1 ambient @p ~ ~ ~ 10 1" > /run/minecraft-stdin.fifo
+printf "%s\n" "execute at @a run playsound minecraft:item.goat_horn.sound.1 ambient @p ~ ~ ~ 10 1" > /run/minecraft-stdin.fifo
 printf "%s\n" "say ${*}" > /run/minecraft-stdin.fifo
 printf "%s\n" "title @a reset" > /run/minecraft-stdin.fifo
 printf "%s\n" "title @a subtitle \"${*}\"" > /run/minecraft-stdin.fifo
 printf "%s\n" "title @a title \"\"" > /run/minecraft-stdin.fifo
+
